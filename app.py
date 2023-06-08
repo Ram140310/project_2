9aa8a025f7dfaaeea3d568306e9bdf00b4c5c054
Ram Chandra <ramchandra@Rams-MacBook-Air.local>
2023-06-08 13:56:03 +0530

    seocnd commitment

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import pandas as pd
 import numpy as np
 app = Flask(__name__)
@@ -14,17 +14,24 @@ def index():
     df=pd.read_csv('trainer.csv')
     options=df['New Trainer'].unique()
     selected_option = options[1]  # Set the desired selected option 
-    return render_template('index1.html',options=options,selected_option=selected_option)
+    return 
+render_template('index1.html',options=options,selected_option=selected_option)
 
-@app.route('/trainer',methods=['GET'])
+@app.route('/trainer',methods=['POST'])
 def trainer():
-    df=pd.read_csv('trainer.csv')
-    return render_template('index.html',df=df)
+    if request.method=='POST':
+        trainer=request.form['trainer']
+        df=pd.read_csv('trainer.csv')
+        df1=df.loc[df['New Trainer'] == trainer]
+        return render_template('index.html',df=df1)
+    else:
+        return render_template('index1.html')
 
-
+@app.route('/trainer/courseupdate')
+def courseupdate():
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='0.0.0.0',port=6969,debug=True)
 
